b63166f51c9f36d98f685810ec2a0558e87d48b3
WardaBar <wardabar95@gmail.com>
2019-01-14 19:49:02 +0100

    Testowanie Test i Character

---
+++ server/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace server.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            //TODO
+            //Sprawdzić czy łaczy się z aplikacją
+            Test test = new Test { Status = true };
+            return Ok(test);
+        }
+    }
+}
---
+++ server/Models/Character.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace server.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Class { get; set; }
+        public Dictionary<string, int> Stats { get; set; }
+        public List<string> Equipment { get; set; }       
+       
+    }
+}
