43786e84f3c66898057965327c2934c4542e9672
Robert Wyszecki <robertwyszecki@gmail.com>
2019-01-22 13:10:23 +0100

    add option map load

--- SuperGra/MainWindow.xaml.cs
+++ SuperGra/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Gma.QrCodeNet.Encoding;
 using Gma.QrCodeNet.Encoding.Windows.Render;
+using Microsoft.Win32;
 using SuperGra.Model;
 using SuperGra.ViewModel;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -14,15 +17,16 @@ namespace SuperGra
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-	{
+    {
         MainViewModel vm = new MainViewModel();
         Data.Repository repository = new Data.Repository();
+        private ImageBrush imagePhoto { get; set; }
 
         public MainWindow()
-		{
-			InitializeComponent();
+        {
+            InitializeComponent();
             DataContext = vm;
-
+            
             vm.AllItems = new ObservableCollection<MyItem>();
             qr_Generate();
         }
@@ -52,6 +56,53 @@ namespace SuperGra
             wRenderer.Draw(wBitmap, qrCode.Matrix);
 
             QrCodeImage.Source = wBitmap;
+        }
+
+        private void bLoadMap_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+
+            if (op.ShowDialog() == true)
+            {
+                var myCanvas = FindVisualChildren<Canvas>(mylist);
+                
+                foreach(var i in myCanvas )
+                {
+                    if (i.Name == "myCanvas")
+                    {
+                        imagePhoto = new ImageBrush();
+                        imagePhoto.ImageSource = new BitmapImage(new Uri(op.FileName, UriKind.Absolute));
+                        i.Background = imagePhoto;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<T> FindVisualChildren<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                {
+                    yield return (T)child;
+                }
+                else
+                {
+                    var childOfChild = FindVisualChildren<T>(child);
+                    if (childOfChild != null)
+                    {
+                        foreach (var subchild in childOfChild)
+                        {
+                            yield return subchild;
+                        }
+                    }
+                }
+            }
         }
     }
 }
