50f299204064edfcfa5f86da64de2eaf1ce28cd7
kaizdraw <wardabar95@gmail.com>
2019-01-22 23:23:39 +0100

    no message

---
+++ server/Controllers/QueueController.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace server.Controllers
+{
+    public class QueueController : ApiController
+    {
+        public static Dictionary<string, Queue<JObject>> queue = new Dictionary<string, Queue<JObject>>();
+
+        [Route("api/queue/GetQueue")]
+        [HttpPost]
+        public IHttpActionResult GetQueue([FromBody] JObject json)
+        {
+            dynamic jsonToId = JsonConvert.DeserializeObject(json.ToString());
+            String id = jsonToId.Id;
+
+            if (!queue.ContainsKey(id))
+            {
+                queue.Add(id, new Queue<JObject>());
+                json.Add("Action", 1);
+                queue["GameMaster"].Enqueue(json);
+            }
+
+            JObject result = new JObject();
+            if(queue[id].Count==0)
+            {
+                result.Add("Action", 0);
+            }
+            else
+            {
+                result = queue[id].Dequeue();
+            }
+
+            return Ok(result);
+        }
+
+        [Route("api/queue/AddQueue")]
+        [HttpPost]
+        public IHttpActionResult AddQueue(JObject json)
+        {
+            bool added = false;
+            dynamic jsonToQueue = JsonConvert.DeserializeObject(json.ToString());
+            String id = jsonToQueue.Id;
+
+            if (queue.ContainsKey(id))
+            {
+                queue[id].Enqueue(json);
+            }
+            else
+            {
+                added = true;
+            }
+
+            Result result = new Result { Status = added };
+
+            return Ok(result);
+        }
+    }
+}
